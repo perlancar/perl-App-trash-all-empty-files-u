59a8e1309c984ef4f79ba8f1babca0cff28a74d2
perlancar (on pc-home) <perlancar@gmail.com>
2020-06-03 14:14:11 +0700

    Initial commit

---
+++ lib/App/trash/all/empty/files/u.pm
@@ -0,0 +1,18 @@
+package App::trash::all::empty::files::u;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Trash all empty files in the current directory tree, with undo/redo capability
+
+=head1 DESCRIPTION
+
+See included script L<trash-all-empty-files-u>.
+
+
+=head1 SEE ALSO
+
+L<File::Trash::EmptyFiles::Undoable>
